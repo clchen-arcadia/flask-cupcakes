a913eada56dc7ca28beaf26ab5f66cd945c0e224
clchen-arcadia <clchen.arcadia@gmail.com>
2022-10-24 15:08:23 -0400

    Add Cupcake to models and boilerplate to app

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
 """Flask app for Cupcakes"""
+
+from flask import (
+    Flask,
+    redirect,
+    render_template
+)
+
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import (
+    db,
+    connect_db,
+    Cupcake
+)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+app.config['SECRET_KEY'] = "DON'T EVER DO THIS IN PRACTICE!!"
+
+# If you don't want intercepted redirects uncomment this line:
+#
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
--- models.py
+++ models.py
@@ -1,1 +1,39 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_CUPCAKE_IMG_URL = "https://tinyurl.com/demo-cupcake"
+
+class Cupcake(db.Model):
+    """Cupcake."""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    flavor = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    size = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    image = db.Column(
+        db.String(500),
+        nullable=False,
+        default=DEFAULT_CUPCAKE_IMG_URL
+    )
